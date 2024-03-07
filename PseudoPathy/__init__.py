bc2c5c3df2073ba09674bd76343f35e01955309c
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-03-07 14:21:58 +0100

    Modulized Path aliases

--- PseudoPathy/__init__.py
+++ PseudoPathy/__init__.py
@@ -1,13 +1,7 @@
 
-try:
-    from PseudoPathy.Paths import Path, FilePath, DirectoryPath, DisposablePath
-    from PseudoPathy.Group import PathGroup
-    from PseudoPathy.Library import MinimalPathLibrary, PathLibrary
-    import PseudoPathy.Functions as Functions
-    import PseudoPathy.Globals as Globals
-except:
-    from Paths import Path, FilePath, DirectoryPath, DisposablePath
-    from Group import PathGroup
-    from Library import MinimalPathLibrary, PathLibrary
-    import Functions as Functions
-    import Globals
+from PseudoPathy.Paths import Path, FilePath, DirectoryPath, DisposablePath
+from PseudoPathy.Group import PathGroup
+from PseudoPathy.Library import MinimalPathLibrary, PathLibrary
+import PseudoPathy.Functions as Functions
+import PseudoPathy.Globals as Globals
+import PseudoPathy.PathShortHands as PathShortHands
