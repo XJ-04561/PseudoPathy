20cf2b9f36512ecfa247b1a4f5bd7d74ae902df5
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-03-14 17:31:42 +0100

    Small changes to `createTemp`

--- PseudoPathy/Functions.py
+++ PseudoPathy/Functions.py
@@ -6,27 +6,27 @@ from PseudoPathy.PathShortHands import *
 from PseudoPathy.Paths import Path, FilePath, DirectoryPath, DisposablePath
 from PseudoPathy.Group import PathGroup
 
-def createTemp(path : Path|PathGroup, prefix : str="", suffix : str="", ext : str=None):
+def createTemp(dir : Path|PathGroup=None, prefix : str=None, suffix : str=None, ext : str=None):
 	"""Creates a temporary directory/file without race-conditions by
 	tempfile.TemporaryDirectory or tempfile.TemporaryFile. To create a file,
 	use the 'ext' keyword argument without a leading '.'."""
-	if type(path) not in [Path, DirectoryPath, FilePath, DisposablePath, PathGroup]:
-		if pIsFile(path):
-			path = FilePath(path)
+	if type(dir) not in [Path, DirectoryPath, FilePath, DisposablePath, PathGroup]:
+		if pIsFile(dir):
+			dir = FilePath(dir)
 		else:
-			path = DirectoryPath(path)
-	elif type(path) is PathGroup:
-		path = path.writable
+			dir = DirectoryPath(dir)
+	elif type(dir) is PathGroup:
+		dir = dir.writeable
 	
 	try:
-		pMakeDirs(path)
+		pMakeDirs(dir)
 	except:
 		return None
 	
 	if ext is None:
-		outPath = DirectoryPath(tempfile.TemporaryDirectory(suffix=suffix, prefix=prefix, dir=path, delete=Globals.DISPOSE))
+		outPath = DirectoryPath(tempfile.TemporaryDirectory(suffix=suffix, prefix=prefix, dir=dir, delete=Globals.DISPOSE))
 	else:
-		outPath = tempfile.TemporaryFile(suffix=suffix+"."+ext, prefix=prefix, dir=path, delete=Globals.DISPOSE)
+		outPath = tempfile.TemporaryFile(suffix=suffix+"."+ext, prefix=prefix, dir=dir, delete=Globals.DISPOSE)
 	
 	return outPath
 
