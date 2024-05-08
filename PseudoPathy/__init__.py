f7ef248733e7957258eb1909751f62dd74f6adae
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-05-08 16:32:27 +0200

    Update __init__.py

--- PseudoPathy/__init__.py
+++ PseudoPathy/__init__.py
@@ -1,6 +1,7 @@
 
 from PseudoPathy.Paths import Path, FilePath, DirectoryPath, PathList
 from PseudoPathy.Group import PathGroup
+from PseudoPathy.PathUtilities import PathProperty, PathAlias
 from PseudoPathy.Library import PathLibrary, SoftwareLibrary
 import PseudoPathy.Functions as Functions
 import PseudoPathy.Globals as Globals
