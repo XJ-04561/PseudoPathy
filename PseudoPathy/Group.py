b183319a5c44ec197ecddd315f556890df9d99f9
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-05-21 14:20:48 +0200

    Update Group.py

--- PseudoPathy/Group.py
+++ PseudoPathy/Group.py
@@ -130,7 +130,7 @@ class PathGroup:
 		if isinstance(other, PathGroup):
 			return self._roots == other._roots
 		elif isinstance(other, str):
-			self._roots == other.split(";")
+			return self._roots == other.split(";")
 		else:
 			return False
 	
